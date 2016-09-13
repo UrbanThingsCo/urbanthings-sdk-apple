3b2793b5d5f99700d265962926d719c1875ff14d
Mark Woollard <mwoollard@newforma.com>
2016-09-13 14:43:58 +0100

    Fix for incorrect colours

--- UrbanThingsAPI/Internal/JSON/UTColor+JSON.swift
+++ UrbanThingsAPI/Internal/JSON/UTColor+JSON.swift
@@ -15,35 +15,32 @@
 #endif
 
 extension UTColor {
-    
-    public class func fromJSON(required required:AnyObject?) throws -> UTColor {
+
+    public class func fromJSON(required required: AnyObject?) throws -> UTColor {
         guard let string = required as? String else {
             throw Error(expected:String.self, not:required, file:#file, function:#function, line:#line)
         }
         guard let value = string.hexValue else {
             throw Error(jsonParseError:"Invalid hex string \(string)", file:#file, function:#function, line:#line)
         }
-        return UTColor(rgb:value)
+
+        #if os(OSX)
+            return UTColor(deviceRed:CGFloat((value & 0xff0000) >> 16)/255.0,
+                      green:CGFloat((value & 0xff00) >> 8)/255.0,
+                      blue:CGFloat(value & 0xff)/255.0,
+                      alpha:1.0)
+        #else
+            return UTColor(red:CGFloat((value & 0xff0000) >> 16)/255.0,
+                      green:CGFloat((value & 0xff00) >> 8)/255.0,
+                      blue:CGFloat(value & 0xff)/255.0,
+                      alpha:1.0)
+        #endif
     }
-    
-    public class func fromJSON(optional optional:AnyObject?) throws -> UTColor? {
+
+    public class func fromJSON(optional optional: AnyObject?) throws -> UTColor? {
         guard let required = optional else {
             return nil
         }
         return try UTColor.fromJSON(required:required)
-    }
-    
-    convenience init(rgb:UInt32) {
-        #if os(OSX)
-            self.init(deviceRed:CGFloat((rgb & 0xff0000) >> 16)/255.0,
-                      green:CGFloat((rgb & 0xff00) >> 8)/255.0,
-                      blue:CGFloat(rgb & 0xff)/255.0,
-                      alpha:1.0)
-        #else
-            self.init(red:CGFloat((rgb & 0xff0000) >> 16)/255.0,
-                      green:CGFloat((rgb & 0xff00) >> 8)/255.0,
-                      blue:CGFloat(rgb & 0xff)/255.0,
-                      alpha:1.0)
-        #endif
     }
 }
